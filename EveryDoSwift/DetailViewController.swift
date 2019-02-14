32a21d8c14605aa5514ff11dc8b22dd903adeaf6
Van Luu <v.luu@hotmail.com>
2019-02-14 11:56:11 -0500

    complete

--- EveryDoSwift/DetailViewController.swift
+++ EveryDoSwift/DetailViewController.swift
@@ -8,33 +8,65 @@
 
 import UIKit
 
+protocol DetailViewControllerDelegate: AnyObject {
+    func saveChanges()
+}
+
 class DetailViewController: UIViewController {
+    
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
 
+    @IBOutlet weak var priorityNumberLabel: UILabel!
+    
+    
+    @IBOutlet weak var toDoDescriptionTextView: UITextView!
+    
 
+    @IBOutlet weak var isCompletedSwitch: UISwitch!
+    
+
+    weak var delegate: DetailViewControllerDelegate?
+    
+    @IBAction func isCompletedSwitchPressed(_ sender: UISwitch) {
+        if let toDo = detailItem {
+            toDo.isCompleted = sender.isOn
+            delegate?.saveChanges()
+        }
+    }
+    
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = detailItem {
+        if let toDo = detailItem {
             if let label = detailDescriptionLabel {
-                label.text = detail.timestamp!.description
+                label.text = toDo.title!.description
+            }
+            if let priorityLabel = priorityNumberLabel {
+                priorityLabel.text = toDo.priorityNumber.description
+            }
+            if let toDoDescription = toDoDescriptionTextView {
+                toDoDescription.text = toDo.todoDescription!.description
+            }
+            if let isCompleted = isCompletedSwitch {
+                isCompleted.isOn = toDo.isCompleted
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
     }
-
-    var detailItem: Event? {
+    
+    var detailItem: ToDo? {
         didSet {
             // Update the view.
             configureView()
         }
     }
-
-
+    
+    
 }
 
--- EveryDoSwift/MasterViewController.swift
+++ EveryDoSwift/MasterViewController.swift
@@ -9,38 +9,15 @@
 import UIKit
 import CoreData
 
-class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
-
+class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate, DetailViewControllerDelegate {
+    
     var detailViewController: DetailViewController? = nil
     var managedObjectContext: NSManagedObjectContext? = nil
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        navigationItem.leftBarButtonItem = editButtonItem
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
-        if let split = splitViewController {
-            let controllers = split.viewControllers
-            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
-        super.viewWillAppear(animated)
-    }
-
-    @objc
-    func insertNewObject(_ sender: Any) {
+    
+    
+    func saveChanges() {
         let context = self.fetchedResultsController.managedObjectContext
-        let newEvent = Event(context: context)
-             
-        // If appropriate, configure the new managed object.
-        newEvent.timestamp = Date()
-
+        
         // Save the context.
         do {
             try context.save()
@@ -51,39 +28,107 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
             fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
         }
     }
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        navigationItem.rightBarButtonItem = addButton
+        if let split = splitViewController {
+            let controllers = split.viewControllers
+            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+            
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+    
+    @objc
+    func insertNewObject(_ sender: Any) {
+        let context = self.fetchedResultsController.managedObjectContext
+        
+        let defaultTitle = UserDefaults.standard.string(forKey: "default_title_for_todo") ?? "Input to do item here"
+        let defaultToDoDescription = UserDefaults.standard.string(forKey: "default_toDoDescription_for_todo") ?? "Input description of your to do item"
+        let defaultPriorityNumber = Int16(UserDefaults.standard.integer(forKey: "default_priorityNumber_for_todo"))
+        
+        let populateController = UIAlertController(title: "Input New To-Do", message: "", preferredStyle: .alert)
+        populateController.addTextField { (titleField: UITextField) in
+            titleField.text = defaultTitle
+        }
+        populateController.addTextField { (toDoDescriptionField: UITextField) in
+            toDoDescriptionField.text = defaultToDoDescription
+        }
+        populateController.addTextField { (priorityNumberField: UITextField) in
+            priorityNumberField.text = defaultPriorityNumber.description
+        }
+        populateController.addAction(UIAlertAction(title: "Add", style: .default, handler: { (sender: UIAlertAction) in
+            
+            
+            let newToDo = ToDo(context: context)
+            newToDo.title = populateController.textFields![0].text ?? defaultTitle
+            newToDo.todoDescription = populateController.textFields![1].text ?? defaultToDoDescription
+            if let priorityString = populateController.textFields![2].text {
+                if let priorityNumber = Int16(priorityString) {
+                    newToDo.priorityNumber = priorityNumber
+                } else {
+                    newToDo.priorityNumber = defaultPriorityNumber
+                }
+            } else {
+                newToDo.priorityNumber = defaultPriorityNumber
+            }
+            
+            
+            // Save the context.
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }))
+        self.present(populateController, animated: true)
+    }
+    
     // MARK: - Segues
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-            let object = fetchedResultsController.object(at: indexPath)
+                let object = fetchedResultsController.object(at: indexPath)
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
+                controller.delegate = self
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return fetchedResultsController.sections?.count ?? 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let sectionInfo = fetchedResultsController.sections![section]
         return sectionInfo.numberOfObjects
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let event = fetchedResultsController.object(at: indexPath)
-        configureCell(cell, withEvent: event)
+        let toDo = fetchedResultsController.object(at: indexPath)
+        configureCell(cell, withToDo: toDo)
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
@@ -93,7 +138,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         if editingStyle == .delete {
             let context = fetchedResultsController.managedObjectContext
             context.delete(fetchedResultsController.object(at: indexPath))
-                
+
             do {
                 try context.save()
             } catch {
@@ -104,25 +149,31 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
             }
         }
     }
-
-    func configureCell(_ cell: UITableViewCell, withEvent event: Event) {
-        cell.textLabel!.text = event.timestamp!.description
+    
+    func configureCell(_ cell: UITableViewCell, withToDo toDo: ToDo) {
+        cell.textLabel!.text = toDo.title!.description
+        cell.detailTextLabel!.text = "\(toDo.priorityNumber) " + toDo.todoDescription!.description
+        if toDo.isCompleted {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
     }
-
+    
     // MARK: - Fetched results controller
-
-    var fetchedResultsController: NSFetchedResultsController<Event> {
+    
+    var fetchedResultsController: NSFetchedResultsController<ToDo> {
         if _fetchedResultsController != nil {
             return _fetchedResultsController!
         }
         
-        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
+        let fetchRequest: NSFetchRequest<ToDo> = ToDo.fetchRequest()
         
         // Set the batch size to a suitable number.
         fetchRequest.fetchBatchSize = 20
         
         // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
         
         fetchRequest.sortDescriptors = [sortDescriptor]
         
@@ -135,57 +186,56 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         do {
             try _fetchedResultsController!.performFetch()
         } catch {
-             // Replace this implementation with code to handle the error appropriately.
-             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-             let nserror = error as NSError
-             fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
         }
         
         return _fetchedResultsController!
-    }    
-    var _fetchedResultsController: NSFetchedResultsController<Event>? = nil
-
+    }
+    var _fetchedResultsController: NSFetchedResultsController<ToDo>? = nil
+    
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.beginUpdates()
     }
-
+    
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
         switch type {
-            case .insert:
-                tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
-            case .delete:
-                tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
-            default:
-                return
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+        default:
+            return
         }
     }
-
+    
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
         switch type {
-            case .insert:
-                tableView.insertRows(at: [newIndexPath!], with: .fade)
-            case .delete:
-                tableView.deleteRows(at: [indexPath!], with: .fade)
-            case .update:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
-            case .move:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
-                tableView.moveRow(at: indexPath!, to: newIndexPath!)
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .update:
+            configureCell(tableView.cellForRow(at: indexPath!)!, withToDo: anObject as! ToDo)
+        case .move:
+            configureCell(tableView.cellForRow(at: indexPath!)!, withToDo: anObject as! ToDo)
+            tableView.moveRow(at: indexPath!, to: newIndexPath!)
         }
     }
-
+    
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.endUpdates()
     }
-
+    
     /*
      // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
      
      func controllerDidChangeContent(controller: NSFetchedResultsController) {
-         // In the simplest, most efficient, case, reload the table view.
-         tableView.reloadData()
+     // In the simplest, most efficient, case, reload the table view.
+     tableView.reloadData()
      }
      */
-
+    
 }
-
